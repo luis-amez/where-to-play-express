954c50eff3e2456542d6c0d2f12b63c2df69c6f6
Luis Amez <luis.amez.vitoria@gmail.com>
2018-03-19 20:06:54 +0100

    Move data from view to controller in locations-list page

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,6 +1,28 @@
 /* GET location list page */
 const locationList = (req, res) => {
-  res.render('locations-list', { title: 'Locations List' });
+  const data = {
+    title: 'Places to play',
+    pageHeader: {
+      title: 'Where to Play',
+      strapline: 'Share your love for board games!'
+    },
+    sidebar: 'Do you want to play? Here you can find the most popular places in your city where the players meet.',
+    locations: [{
+      name: 'Juegamestore',
+      address: 'C/ Falsa, 123, Cullera',
+      rating: 5,
+      facilities: ['Food', 'Store', 'Wifi'],
+      distance: '500m'
+    }, {
+      name: 'Kaburi',
+      address: 'C/ Truco, 13, Barcelona',
+      rating: 4,
+      facilities: ['Store', 'Bathroom'],
+      distance: '100m'
+    }]
+  };
+
+  res.render('locations-list', data);
 };
 
 /* GET location info page */
