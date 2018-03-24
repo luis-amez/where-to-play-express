3f3bd2c8ac2d108804a1e52a0ecaae5ca46d43ed
Luis Amez <luis.amez.vitoria@gmail.com>
2018-03-24 12:51:27 +0100

    Move data from view to controller in about page

--- controllers/others.js
+++ controllers/others.js
@@ -5,7 +5,12 @@ const index = (req, res) => {
 
 /* GET about page */
 const about = (req, res) => {
-  res.render('about', { title: 'About' });
+  const data = {
+    title: 'About Where to Play',
+    content: 'Where to Play helps you to find the best places to enjoy your favourite hobby: Playing board games!\nLorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  };
+
+  res.render('about', data);
 };
 
 module.exports = {
